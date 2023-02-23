946d06d1826661f685f321901fa1b35580f79e19
AnanyaaKoundal <ananyaa.koundal2003@gmail.com>
2023-02-24 01:01:23 +0530

    signup-login page attached

--- app.py
+++ app.py
@@ -23,26 +23,44 @@ config.gpu_options.per_process_gpu_memory_fraction = 0.2
 config.gpu_options.allow_growth = True
 session = InteractiveSession(config=config)
 # Keras
-from tensorflow.keras.applications.resnet50 import preprocess_input
+# from tensorflow.keras.applications.resnet50 import preprocess_input
 from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing import image
 
 # Flask utils
 from flask import Flask, redirect, url_for, request, render_template
 from werkzeug.utils import secure_filename
+from datetime import datetime
+import sqlite3
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 #from gevent.pywsgi import WSGIServer
 
 # Define a flask app
 app = Flask(__name__)
+app.app_context().push()
+
+# Initialising database Users.db
+app.config['SQLALCHEMY_DATABASE_URI']="sqlite:///Users.db"
+app.config['SECRETKEY']="plantdiseaseproject"
+db=SQLAlchemy(app)
+bcrypt=Bcrypt(app)
+
+# Creating table members
+class Members(db.Model):
+    email=db.Column(db.String(50), nullable=False, primary_key=True)
+    username=db.Column(db.String(50), nullable=False)
+    password=db.Column(db.String(50), nullable=False)
+    date_registered=db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self) ->str:
+        return f"{self.email}"
 
 # Model saved with Keras model.save()
 MODEL_PATH ='InceptionModel.h5'
 
 # Load your trained model
 model = load_model(MODEL_PATH)
-
-
-
 
 def model_predict(img_path, model):
     print(img_path)
@@ -89,7 +107,6 @@ def index():
     # Main page
     return render_template('index.html')
 
-
 @app.route('/predict', methods=['GET', 'POST'])
 def upload():
     if request.method == 'POST':
@@ -108,6 +125,56 @@ def upload():
         return result
     return None
 
+@app.route('/signup.html', methods=['GET', 'POST'])
+def signup():
+    if request.method == 'POST':
+        email=request.form['email']
+        user=request.form['username']
+        pas=request.form['password']
+        conn=sqlite3.connect('instance/users.db')
+        con=conn.cursor()
+        stat=f"SELECT * FROM members WHERE username='{user}' or email='{email}'"
+        con.execute(stat)
+        if con.fetchone():
+            return render_template('signup.html', err=True)
+        hashed_pas=bcrypt.generate_password_hash(pas)
+        e=Members(email=email, username=user, password=hashed_pas)
+        db.session.add(e)
+        db.session.commit()
+        entry=Members.query.all()
+        print('Abc')
+        return render_template('index.html', entry=entry)
+    return render_template('signup.html')
+
+@app.route('/login.html', methods=['GET', 'POST'])
+def login():
+    return render_template('login.html')
+
+
+@app.route('/loginval', methods=['GET', 'POST'])
+def logcheck():
+    ac=request.form['ans']
+    pas=request.form['password']
+    check=bcrypt.generate_password_hash(pas)
+    conn=sqlite3.connect('instance/users.db')
+    con=conn.cursor()
+    err=False
+    stat=f"SELECT * FROM members WHERE username='{ac}'"
+    con.execute(stat)
+    if con.fetchone():
+        stat=f"SELECT password FROM members WHERE username='{ac}'"
+        con.execute(stat)
+        if bcrypt.check_password_hash(con.fetchone()[0], pas):
+            return render_template('index.html')
+        else:
+            err=True
+    else:
+        err=True
+    return render_template('login.html', err=err)
+
+# @app.route('/home.html')
+# def home():
+#     return render_template('home.html')
 
 if __name__ == '__main__':
     app.run(port=5001,debug=True)
